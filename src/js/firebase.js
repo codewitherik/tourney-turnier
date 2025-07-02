5306edd57f1b93a0d5bed6337e1e2ab893a5f36c
codewitherik <codewitherik>
2025-07-02 11:55:59 +0200

    Firebase

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -4,7 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-
+const firebaseConfig = {
+  apiKey: "AIzaSyB4lE7AatRUXt0tSKsNRrXn1N94c4gn60s",
+  authDomain: "tourney-turnieplaner.firebaseapp.com",
+  projectId: "tourney-turnieplaner",
+  storageBucket: "tourney-turnieplaner.firebasestorage.app",
+  messagingSenderId: "543729051769",
+  appId: "1:543729051769:web:96c8d752e98e2b1640cca8"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
